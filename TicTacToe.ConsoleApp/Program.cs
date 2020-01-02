151f2285c4f3c227a86d99c3edd14e000b7db5c3
Melva Lang <mjlang74@gmail.com>
2020-01-01 23:23:05 -0600

    Created new project

--- TicTacToe.ConsoleApp/Program.cs
+++ TicTacToe.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace TicTacToe.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
